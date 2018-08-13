1cb35fa6aabb8c284fb258814d16fa39626317d1
John Todd <jqtodd@verisk.com>
2018-08-13 17:05:18 -0600

    It's always possible the user doesn't want the ip visible...

--- config.js
+++ config.js
@@ -3,7 +3,8 @@ var config = {
     db_host:"localhost",
     db_port:27017,
     db_name:"main",
-    uploads_path: "uploads/", 
+    uploads_path: "uploads/",
+    show_address: true,
     slide_timeout: 60, 
     admin_users:{'admin':'Password',
 		 'user': 'Password'}
